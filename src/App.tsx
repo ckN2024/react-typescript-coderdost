6ca96819ba59a359f8ce4e781c6a6e53430f74e4
ck <cktyloones@gmail.com>
2024-03-17 19:59:14 +0530

    video 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Note from './components/note/Note';
+import { Notes } from './components/note/data'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>React with typescript</h2>
+      <div>
+        {
+          Notes.map((note) => (
+            <Note key={note.id} text={note.text} priority={note.priority}></Note>
+          ))
+        }
+      </div>
     </div>
   );
 }
---
+++ src/components/note/Note.tsx
@@ -0,0 +1,12 @@
+import "./note.css"
+import {NoteProps} from './notes-type'
+
+function Note(props: NoteProps) {
+  return (
+    <div className={`note ${props.priority}`}>
+      {props.text}
+    </div>
+  )
+}
+
+export default Note;
